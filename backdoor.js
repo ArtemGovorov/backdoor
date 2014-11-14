fc537c081a3e92a22f402b580c85d4aa738688b5
Artem Govorov <artem.govorov@gmail.com>
2014-11-14 13:16:50 +1000

    Fixed named backdoor assigment

--- backdoor.js
+++ backdoor.js
@@ -3,7 +3,7 @@
   root.backdoor = function (evaluator) {
     var name = evaluator.name;
     if (!name) throw new Error('backdoor argument function must have a name');
-    root.backdoor[name] = root.backdoor[name] = evaluator;
+    root.backdoor[name] = root.backdoor[name] || evaluator;
   };
 
 }.call(this));
